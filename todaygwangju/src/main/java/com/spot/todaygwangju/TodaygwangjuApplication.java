0f6dc82833f9cde36055ba7d90a4e7c7289c9ab0
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-06-23 20:19:01 +0900

    init: 초기 파일 생성

---
+++ todaygwangju/src/main/java/com/spot/todaygwangju/TodaygwangjuApplication.java
@@ -0,0 +1,13 @@
+package com.spot.todaygwangju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodaygwangjuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodaygwangjuApplication.class, args);
+    }
+
+}
---
+++ todaygwangju/src/test/java/com/spot/todaygwangju/TodaygwangjuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spot.todaygwangju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodaygwangjuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
