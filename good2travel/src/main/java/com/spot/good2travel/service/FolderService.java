0e11706ac3c0f13d2c767d433294235db8f50743
X1n9fU <minkyeong0244@gmail.com>
2024-09-07 17:03:15 +0900

    [docs] todo 이후 유저 Join #10

--- good2travel/src/main/java/com/spot/good2travel/service/FolderService.java
+++ good2travel/src/main/java/com/spot/good2travel/service/FolderService.java
@@ -2,7 +2,6 @@ package com.spot.good2travel.service;
 
 import com.spot.good2travel.domain.Folder;
 import com.spot.good2travel.dto.FolderRequest;
-import com.spot.good2travel.dto.FolderResponse;
 import com.spot.good2travel.repository.FolderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,13 +13,13 @@ public class FolderService {
     private final FolderRepository folderRepository;
 
     /*
-    새 폴더 만들기 :
+    새 폴더 만들기
      */
     public void create(FolderRequest.FolderCreate folderRequest) {
         Folder newFolder = Folder.builder()
                 .title(folderRequest.getTitle())
+//                .user() todo 유저 추가
                 .build();
         folderRepository.save(newFolder);
-
     }
 }
