b56245e89d9a6b79c8f5a40f8f856a1b55046e29
X1n9fU <minkyeong0244@gmail.com>
2024-09-07 16:56:45 +0900

    [feat] @Builder 추가 #10

--- good2travel/src/main/java/com/spot/good2travel/domain/Folder.java
+++ good2travel/src/main/java/com/spot/good2travel/domain/Folder.java
@@ -1,14 +1,14 @@
 package com.spot.good2travel.domain;
 
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.List;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Folder {
 
