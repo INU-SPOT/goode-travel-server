a774072b134f7b7a07d95ede7a78efc61939f70a
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-09-16 16:27:03 +0900

    refactor: 디버깅을 위해 적어놨던 log 삭제#17

--- good2travel/src/main/java/com/spot/good2travel/common/security/JwtAuthFilter.java
+++ good2travel/src/main/java/com/spot/good2travel/common/security/JwtAuthFilter.java
@@ -26,17 +26,14 @@ public class JwtAuthFilter extends OncePerRequestFilter {
                                     HttpServletResponse servletResponse,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        log.info("[doFilerInternal] 토큰 얻어오기");
         String token = jwtProvider.getAuthorizationToken(servletRequest);
 
         log.info("[doFilterInternal] 토큰 얻어오기 성공");
-        log.info("[doFilterInternal] Token ={}", token);
 
         if (token != null) {
             String jwtToken = token.substring(7);
             log.info("[doFilterInternal] jwtToken:{}", jwtToken);
 
-            log.info("[doFilterInternal] 토큰 타입 확인");
             if (servletRequest.getRequestURI().equals("/v1/auth/reissue")) {
                 jwtProvider.validRefreshToken(jwtToken);
             } else {
