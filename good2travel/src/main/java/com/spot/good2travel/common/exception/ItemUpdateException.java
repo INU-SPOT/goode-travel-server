b23441584e1255e8e904c1f86dcf6a351fff18f7
X1n9fU <minkyeong0244@gmail.com>
2024-09-18 01:18:10 +0900

    [style] Exception 이름 변경 #14

--- good2travel/src/main/java/com/spot/good2travel/common/exception/ItemUpdateException.java
+++
@@ -1,7 +0,0 @@
-package com.spot.good2travel.common.exception;
-
-public class ItemUpdateException extends RuntimeException{
-    public ItemUpdateException(ExceptionMessage m){
-        super(m.getMessage());
-    }
-}
