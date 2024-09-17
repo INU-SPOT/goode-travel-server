cdb63b35583d7b9ab122548a22610fa43b979065
X1n9fU <minkyeong0244@gmail.com>
2024-09-18 01:18:10 +0900

    [style] Exception 이름 변경 #14

--- good2travel/src/main/java/com/spot/good2travel/common/exception/GlobalExceptionHandler.java
+++ good2travel/src/main/java/com/spot/good2travel/common/exception/GlobalExceptionHandler.java
@@ -71,8 +71,8 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(CommonResponse.error(ex.getMessage(), null));
     }
 
-    @ExceptionHandler(value = ItemUpdateException.class)
-    public ResponseEntity<CommonResponse<?>> handleItemUpdateException(ItemUpdateException ex){
+    @ExceptionHandler(value = ItemAccessException.class)
+    public ResponseEntity<CommonResponse<?>> handleItemUpdateException(ItemAccessException ex){
         log.error("[handleItemUpdateException] {}", ex.getMessage());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(CommonResponse.error(ex.getMessage(), null));
     }
