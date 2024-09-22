e00c23e64333b9ca278d5263efb7cef2e7bd256d
X1n9fU <minkyeong0244@gmail.com>
2024-09-22 14:50:31 +0900

    [refactor] Response Swagger 설명 및 pk 추가 #30

--- good2travel/src/main/java/com/spot/good2travel/common/fcm/FcmRequest.java
+++ good2travel/src/main/java/com/spot/good2travel/common/fcm/FcmRequest.java
@@ -1,6 +1,7 @@
 package com.spot.good2travel.common.fcm;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
@@ -30,6 +31,7 @@ public class FcmRequest {
     @Setter
     public static class FcmUpdateDto {
         @Schema(example = "eMogIDKaPnLC6jUJtNeIfn:APA91bGP3G6gUng1cvA-YvMLtPIw90Eqlb30fP9vYYO22wfPQMKKigEXcKGFIVEuPPln23xPSZOqs8WaYWik6PWuOxOLrvUTV5E32jdpLyVS4G5YDpg_2bZf5ITKmpiWmHhfZk6phxxN")
+        @NotBlank(message = "발급받은 fcm 토큰을 입력해주세요.")
         private String fcmToken;
     }
 }
--- good2travel/src/main/java/com/spot/good2travel/dto/AlarmResponse.java
+++ good2travel/src/main/java/com/spot/good2travel/dto/AlarmResponse.java
@@ -1,6 +1,7 @@
 package com.spot.good2travel.dto;
 
 import com.spot.good2travel.domain.Alarm;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,12 +10,21 @@ import java.time.LocalDateTime;
 @Getter
 public class AlarmResponse {
 
-    private String title;
-    private String message;
-    private LocalDateTime localDateTime;
+    @Schema(example = "1")
+    private final Long id;
+
+    @Schema(example = "깨구링님이 '환기리의 꿀잼 인천여행' 게시물에 댓글을 달았어요.")
+    private final String title;
+
+    @Schema(example = "아 ㅋㅋ 내용 개구린데요?")
+    private final String message;
+
+    @Schema(example = "2024-09-21T22:25:39.03422")
+    private final LocalDateTime localDateTime;
 
     @Builder
-    public AlarmResponse(String title, String message, LocalDateTime localDateTime) {
+    public AlarmResponse(Long id, String title, String message, LocalDateTime localDateTime) {
+        this.id = id;
         this.title = title;
         this.message = message;
         this.localDateTime = localDateTime;
@@ -22,6 +32,7 @@ public class AlarmResponse {
 
     public static AlarmResponse of(Alarm alarm){
         return AlarmResponse.builder()
+                .id(alarm.getId())
                 .title(alarm.getTitle())
                 .message(alarm.getMessage())
                 .localDateTime(alarm.getLocalDateTime())
