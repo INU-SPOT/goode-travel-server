536a34ca4e0c4959138f29fc02584d1809c707de
김정아 <92552047+squidjiny@users.noreply.github.com>
2024-09-27 06:07:00 +0900

    feat: course 초기 설정#26

---
+++ good2travel/src/main/java/com/spot/good2travel/service/CourseService.java
@@ -0,0 +1,9 @@
+package com.spot.good2travel.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CourseService {
+}
