2f1e602651f9e38f2b21a0eab32b0b7c679504cb
X1n9fU <minkyeong0244@gmail.com>
2024-09-27 00:35:18 +0900

    [fix] fcm Message에 putData로 postId 추가 #31

--- good2travel/src/main/java/com/spot/good2travel/controller/FcmController.java
+++ good2travel/src/main/java/com/spot/good2travel/controller/FcmController.java
@@ -52,7 +52,7 @@ public class FcmController {
             @ApiResponse(responseCode = "201", description = "메세지 생성 완료", content = @Content(schema = @Schema(implementation = String.class))),
     })
     public ResponseEntity<CommonResponse<?>> sendMessage(@RequestBody FcmRequest.FcmMessage fcmRequest) throws FirebaseMessagingException {
-        String response = fcmService.sendMessage(fcmRequest.getFcmToken(), fcmRequest.getTitle(), fcmRequest.getBody(), 2L);
+        String response = fcmService.sendMessage(fcmRequest.getFcmToken(), fcmRequest.getTitle(), fcmRequest.getBody(), null);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(CommonResponse.success("send message", response));
     }
