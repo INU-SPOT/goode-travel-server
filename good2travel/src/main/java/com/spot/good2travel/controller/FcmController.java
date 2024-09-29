673f67fe944d1a1220c0e9c1b61e635529c82957
X1n9fU <minkyeong0244@gmail.com>
2024-09-29 14:57:31 +0900

    [fix] fcm 토큰 업데이트 할 시에 토큰의 오류가 있다면 반환  #67

--- good2travel/src/main/java/com/spot/good2travel/controller/FcmController.java
+++ good2travel/src/main/java/com/spot/good2travel/controller/FcmController.java
@@ -28,14 +28,14 @@ public class FcmController {
     @PostMapping("/v1/fcm")
     @Operation(
             summary = "fcm 토큰 저장 및 업데이트",
-            description = "fcm 토큰을 저장하거나 업데이트 합니다" +
+            description = "fcm 토큰을 저장하거나 업데이트 합니다. 그와 동시에 fcm 토큰의 유효성을 체크하고 오류가 있을 경우 오류응답을 내보냅니다." +
                     "<br><br> - request : accessToken 헤더 추가, fcmUpdateDto" +
-                    "<br><br> - response : x"
+                    "<br><br> - response : 저장 및 업데이트 - 201, fcm 토큰의 오류 - 4xx 과 에러 내용 반환"
     )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "토큰 저장 완료", content = @Content(schema = @Schema(implementation = Null.class))),
     })
-    public ResponseEntity<CommonResponse<?>> updateToken(@RequestBody FcmRequest.FcmUpdate fcmUpdate, @AuthenticationPrincipal UserDetails userDetails){
+    public ResponseEntity<CommonResponse<?>> updateToken(@RequestBody FcmRequest.FcmUpdate fcmUpdate, @AuthenticationPrincipal UserDetails userDetails) throws FirebaseMessagingException {
         fcmService.updateToken(fcmUpdate, userDetails);
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(CommonResponse.success("토큰 저장 완료", null));
@@ -46,13 +46,15 @@ public class FcmController {
             summary = "fcm 전송 (테스트 용, 실제 사용은 하지 않는다)",
             description = "fcm을 전송합니다." +
                     "<br><br> - request : FcmMessageDto" +
-                    "<br><br> - response : projects/goode2travel-fcm/messages/**"
+                    "<br> dryRun이 true인 경우 테스트, false인 경우 실제 전송된다." +
+                    "<br><br> - response : " +
+                    "<br> 성공 시 : projects/goode2travel-fcm/messages/**, 실패 시 : 에러"
     )
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "메세지 생성 완료", content = @Content(schema = @Schema(implementation = String.class))),
     })
     public ResponseEntity<CommonResponse<?>> sendMessage(@RequestBody FcmRequest.FcmMessage fcmRequest) throws FirebaseMessagingException {
-        String response = fcmService.sendMessage(fcmRequest.getFcmToken(), fcmRequest.getTitle(), fcmRequest.getBody(), null);
+        String response = fcmService.sendMessage(fcmRequest.getFcmToken(), fcmRequest.getTitle(), fcmRequest.getBody(), null, fcmRequest.getDryRun());
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(CommonResponse.success("send message", response));
     }
--- good2travel/src/main/java/com/spot/good2travel/service/FcmService.java
+++ good2travel/src/main/java/com/spot/good2travel/service/FcmService.java
@@ -31,7 +31,7 @@ public class FcmService {
     private final UserRepository userRepository;
     private final NotificationRepository notificationRepository;
 
-    public String sendMessage(String token, String title, String body, Long postId) throws FirebaseMessagingException {
+    public String sendMessage(String token, String title, String body, Long postId, Boolean dryRun) throws FirebaseMessagingException {
         Message message = Message.builder()
                 .setToken(token)
                 .setWebpushConfig(WebpushConfig.builder().putHeader("ttl", "300")
@@ -40,11 +40,11 @@ public class FcmService {
                         .build())
                 .build();
 
-        return FirebaseMessaging.getInstance().send(message);
+        return FirebaseMessaging.getInstance().send(message, dryRun);
     }
 
     @Transactional
-    public void updateToken(FcmRequest.FcmUpdate fcmUpdate, UserDetails userDetails){
+    public void updateToken(FcmRequest.FcmUpdate fcmUpdate, UserDetails userDetails) throws FirebaseMessagingException {
         Long id = ((CustomUserDetails) userDetails).getId();
         Optional<Fcm> fcm = fcmRepository.findByUserId(id);
         if (fcm.isPresent()) {
@@ -54,20 +54,21 @@ public class FcmService {
                     .orElseThrow(() -> new NotFoundElementException(ExceptionMessage.USER_NOT_FOUND));
             fcmRepository.save(Fcm.of(fcmUpdate.getFcmToken(), user));
         }
+        sendMessage(fcmUpdate.getFcmToken(),  null, null, null, true); //토큰 유효성 검사
     }
 
     public void sendMessageForComment(User user, Post post, String token, CommentRequest.CommentCreateRequest request, LocalDateTime notificationTime) throws FirebaseMessagingException {
         String title = user.getNickname() + "님이 '" + post.getTitle()+"' 게시물에 댓글을 달았어요.";
         String body = request.getContent();
         notificationRepository.save(Notification.of(post.getId(), title, body, notificationTime, post.getUser()));
-        sendMessage(token,title, body, post.getId());
+        sendMessage(token,title, body, post.getId(), false);
     }
 
     public void sendMessageForReplyComment(User user, Post post, String token, CommentRequest.ReplyCommentCreateRequest request, LocalDateTime notificationTime) throws FirebaseMessagingException {
         String title = user.getNickname() + "님이 내 댓글에 대댓글을 달았어요.";
         String body = request.getContent();
         notificationRepository.save(Notification.of(post.getId(), title, body, notificationTime, post.getUser()));
-        sendMessage(token, title, body, post.getId());
+        sendMessage(token, title, body, post.getId(), false);
     }
 
 }
